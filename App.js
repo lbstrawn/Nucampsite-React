1fff2e910f0ef3fdb6d131f38ed156510ec73828
Brad Strawn <lbstrawn@live.com>
2020-09-12 19:43:26 -0500

    Add 5 files

---
+++ App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import {Navbar, NavbarBrand} from 'reactstrap';
+import Main from './components/mainComponent';
+import './App.css';
+
+
+class App extends Component {
+  render() {
+      return (
+        <div className="App">
+              <Main />
+        </div>
+      );
+  }
+}
+
+export default App;
